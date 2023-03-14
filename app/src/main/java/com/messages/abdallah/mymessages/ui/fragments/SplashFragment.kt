534bb90bede7bfac2af70da48a7ec54ad49d394d
MohammedOss <abdallahomar1992@gmail.com>
2023-03-14 17:02:16 +0300

    anim

--- app/src/main/java/com/messages/abdallah/mymessages/ui/fragments/SplashFragment.kt
+++ app/src/main/java/com/messages/abdallah/mymessages/ui/fragments/SplashFragment.kt
@@ -3,13 +3,13 @@ package com.messages.abdallah.mymessages.ui.fragments
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.messages.abdallah.mymessages.R
@@ -42,13 +42,23 @@ class SplashFragment : Fragment() {
 
 //        tvSplash.animate().translationXBy(-1000f).duration=3000
 //        tvSplash.animate().alpha(0.5f)
-        tvSplash.animate().translationYBy(1000f).rotation(360f).duration=3000
-        imgSplash.animate().rotation(360f).duration=3000
+//        tvSplash.animate().translationYBy(1000f).rotation(360f).duration=3000
+//        imgSplash.animate().rotation(360f).duration=3000
 //        tvSplash.startAnimation(slideAnimation)
+
+        var uptodown = AnimationUtils.loadAnimation(requireContext(), R.anim.uptodown)
+        var downtoup = AnimationUtils.loadAnimation(requireContext(), R.anim.downtoup)
+
+        tvSplash.animate(). rotation(360f).duration=3000
+        imgSplash.animate().rotation(360f).duration=3000
+
+        tvSplash.setAnimation(downtoup)
+        imgSplash.setAnimation(uptodown)
 
         Handler(Looper.myLooper()!!).postDelayed({
 //            val direction = SplashFragmentDirections.actionSplashFragmentToFirsFragment()
 //            findNavController().navigate(direction)
+
             findNavController()
                 .navigate(R.id.action_splashFragment_to_firsFragment,
                     null,
